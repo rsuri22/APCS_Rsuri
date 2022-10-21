661f2029c288e289cb4e02f50cc715117532be9e
rsuri22 <rohansuri.prime@gmail.com>
2022-10-21 15:09:34 -0500

    Added findElement feature given atomic symbol.

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Element> elements = readElementFromCSV("/Users/rohansuri/Downloads/periodicdata.csv");
+        System.out.println("What do you need help with?\n\t1: Information about an element\n\t2: Molar Mass Calculation ");
+        String ans = sc.nextLine();
         boolean keepGoing = true;
-        while (keepGoing) {
-            System.out.println("What molecule would you like to find the molar mass of? Type STOP to end.");
-            String userInput = sc.nextLine();
-            if (userInput.equals("STOP") || userInput.equals("stop"))
-                keepGoing = false;
-            else {
-                System.out.println(Element.calculateMolarMass(elements, userInput));
+        if (ans.equals("1")){
+            while (keepGoing) {
+                System.out.println("What element would you like information for? Enter atomic symbol please (type STOP to end): ");
+                String symbol = sc.nextLine();
+                if (symbol.equals("STOP") || symbol.equals(("stop")))
+                    keepGoing = false;
+                else {
+                    System.out.println(Element.findBySymbol(elements, symbol));
+                }
             }
         }
-        System.out.println("Thanks for using my molar mass calculator!");
-//        System.out.println("What element's information would you like? Enter atomic symbol: ");
-//        String symbol = sc.nextLine();
-//        System.out.println(Element.findBySymbol(elements, symbol));
+
+        else if (ans.equals("2")) {
+            while (keepGoing) {
+                System.out.println("What molecule would you like to find the molar mass of? Type STOP to end.");
+                String userInput = sc.nextLine();
+                if (userInput.equals("STOP") || userInput.equals("stop"))
+                    keepGoing = false;
+                else {
+                    System.out.println(Element.calculateMolarMass(elements, userInput));
+                }
+            }
+            System.out.println("Thanks for using my molar mass calculator!");
+        }
+
+        else
+            System.out.println("Sorry, wrong answer. Goodbye!");
+
+        sc.close();
 
     }
 
