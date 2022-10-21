c536587d9895727ecad8599c15b9977c85e501ee
rsuri22 <rohansuri.prime@gmail.com>
2022-10-21 14:50:34 -0500

    MM calculator almost fully functional, need to add parentheses compatibility

--- src/Element.java
+++ src/Element.java
@@ -18,21 +18,68 @@ public class Element {
     }
     public static double calculateMolarMass(List<Element> elements, String molecule){
         double molecularMass = 0;
+        molecule += "?";
         for (int i = 0; i < molecule.length(); i++){
             char c = molecule.charAt(i);
-            if (Character.isDigit(c)) {
-                int numOfE = Character.getNumericValue(c);
-                String currentE = "";
-                if (Character.isUpperCase(molecule.charAt(i - 1))){
-                    currentE += molecule.charAt(i - 1);
+            if (Character.isUpperCase(c)){
+                boolean runLoop = true;
+                int j = i;
+                while (runLoop){
+                    j++;
+                    if (j >= molecule.indexOf("?"))
+                        runLoop = false;
+
+                    else if (Character.isUpperCase(molecule.charAt(j)))
+                        runLoop = false;
+                }
+//                boolean hasInt = false;
+//                char[] chars = molecule.substring(i, j).toCharArray();
+//                for (char a: chars){
+//                    if (Character.isDigit(a))
+//                        hasInt = true;
+//                }
+                boolean nextChar = true;
+                if (molecule.charAt(i + 1) == '?'){
+                    nextChar = false;
+
+                }
+                 if (!nextChar || i == j - 1){
+                    Element e = findBySymbol(elements, molecule.substring(i, i + 1));
+                    int numOfE = 1;
+                    molecularMass += e.getAtomicMass() * numOfE;
+                }
+                else if (Character.isDigit(molecule.charAt(i + 1)) && nextChar) {
+                    Element e = findBySymbol(elements, molecule.substring(i, i + 1));
+                    int numOfE = Integer.parseInt(molecule.substring(i + 1, j));
+                    molecularMass += e.getAtomicMass() * numOfE;
+                }
+
+                else if (Character.isDigit(molecule.charAt(i + 2))){
+                    Element e = findBySymbol(elements, molecule.substring(i, i + 2));
+                    int numOfE = Integer.parseInt(molecule.substring(i + 2, j));
+                    molecularMass += e.getAtomicMass() * numOfE;
                 }
                 else {
-                    currentE += molecule.substring(i - 2, i);
-                }
-                Element e = findBySymbol(elements, currentE);
-                molecularMass += e.getAtomicMass() * numOfE;
+                    Element e = findBySymbol(elements, molecule.substring(i, i + 2));
+                    int numOfE = 1;
+                    molecularMass += e.getAtomicMass() * numOfE;
+                 }
+
 
             }
+//            if (Character.isDigit(c)) {
+//                int numOfE = Character.getNumericValue(c);
+//                String currentE = "";
+//                if (Character.isUpperCase(molecule.charAt(i - 1))){
+//                    currentE += molecule.charAt(i - 1);
+//                }
+//                else {
+//                    currentE += molecule.substring(i - 2, i);
+//                }
+//                Element e = findBySymbol(elements, currentE);
+//                molecularMass += e.getAtomicMass() * numOfE;
+//
+//            }
         }
          return molecularMass;
     }
