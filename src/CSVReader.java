f622f77934dbd247052ed78247bf6792f8d1a769
rsuri22 <rohansuri.prime@gmail.com>
2022-10-25 19:11:41 +0000

    deleted unnecessary imports

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -6,7 +6,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class CSVReader {
     private static Element createElement(String[] scrapedData){
--- src/Element.java
+++ src/Element.java
@@ -1,6 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.List;
-import java.util.ArrayList;
 
 public class Element {
     int atomicNumber;
