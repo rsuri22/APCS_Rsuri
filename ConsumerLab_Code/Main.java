f2bcca2519390a53e278e0cecf8ab414ee0c0d5f
rsuri22 <rohansuri.prime@gmail.com>
2022-11-16 20:02:36 +0000

    Deleted extra files

--- ConsumerLab_Code/Main.java
+++
@@ -1,9 +0,0 @@
-package ConsumerLab_Code;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println(Review.starRating("/workspace/APCS_Rsuri/ConsumerLab_Code/SimpleReview.txt"));
-        
-
-    }
-}
