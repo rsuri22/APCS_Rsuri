7d0faf0ecbd85ebff80d45f09f539e1f4e52ac54
rsuri22 <rsuri22@ladueschools.net>
2023-01-09 01:27:52 +0000

    Finished

---
+++ Activity374/Main.java
@@ -0,0 +1,42 @@
+package Activity374;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        HorseBarn h = new HorseBarn();
+        ArrayList<Horse> randomHorses = randomizeHorses(h);
+        pairHorses(randomHorses);
+//        System.out.println(randomHorses.toString());
+
+    }
+
+    public static ArrayList<Horse> randomizeHorses(HorseBarn h){
+        int[] randNums = RandomPermutation.next(h.getSpaces().size());
+        ArrayList<Horse> randomHorses = new ArrayList<Horse>();
+
+        for (int i = 0; i < h.getSpaces().size(); i++){
+            randomHorses.add(h.getSpaces().get(randNums[i]));
+        }
+
+        return randomHorses;
+    }
+
+    public static void pairHorses(ArrayList<Horse> randomHorses){
+        if (randomHorses.size() % 2 == 1){ 
+            System.out.println(randomHorses.get(randomHorses.size() - 1) + " is the only horse without a partner. The rest of the partners are displayed below:");
+        }
+        for (int i = 0; i < randomHorses.size(); i += 2) {
+            if (i == randomHorses.size()-1){
+                break;
+            }
+            System.out.println("Pair: " + randomHorses.get(i).toString() + " and " + randomHorses.get(i+1).toString());
+            
+
+        }
+
+
+
+    }
+}
---
+++ Activity374/RandomPermRunner.java
@@ -0,0 +1,10 @@
+package Activity374;
+
+public class RandomPermRunner{
+  public static void main(String[] args)
+  {
+    int[] randNums = RandomPermutation.next(10);
+     
+    System.out.println(java.util.Arrays.toString(randNums));
+  }
+}
